a132d8fc8df417b257ffbf93b53cda1b580698ae
JeromeBadt <jb163@hdm-stuttgart.de>
2016-11-27 19:27:05 +0100

    Add a few missing semicolons and move styles.css to css folder

--- webserver.js
+++ webserver.js
@@ -2,13 +2,13 @@
  * Created by fuexle on 05.10.2016.
  * Source; https://blog.risingstack.com/your-first-node-js-http-server
  */
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (request, response) => {
     response.send('Hello from Express!')
-})
+});
 
 app.listen(port, (err) => {
     if (err) {
@@ -16,4 +16,4 @@ app.listen(port, (err) => {
     }
 
     console.log(`server is listening on ${port}`)
-})
+});
